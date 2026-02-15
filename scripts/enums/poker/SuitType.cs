93bce3e0d27868329a064d50a84db78a9c220ee6
Yumei1010 <3062181720@qq.com>
2026-02-15 11:43:11 +0800

    feat(scripts):添加扑克花色枚举

---
+++ scripts/enums/poker/SuitType.cs
@@ -0,0 +1,26 @@
+﻿//     定义卡牌花色类型的键值枚举
+namespace GFrameworkGodotTemplate.scripts.enums.poker;
+
+/// <summary>
+///     定义卡牌花色类型的键值枚举
+///     用于标识和管理不同的卡牌花色类型
+/// </summary>
+public enum SuitType
+{
+    /// <summary>
+    ///     红桃
+    /// </summary>
+    Heart,
+    /// <summary>
+    ///     方块
+    /// </summary>
+    Diamond,
+    /// <summary>
+    ///     黑桃
+    /// </summary>
+    Spade,
+    /// <summary>
+    ///     梅花
+    /// </summary>
+    Club
+}
