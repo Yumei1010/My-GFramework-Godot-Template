7078a26a06845283b79af1b9e31aa2e0fe0f09b9
Yumei1010 <3062181720@qq.com>
2026-02-15 11:42:43 +0800

    feat(scripts):添加扑克数值类型枚举

---
+++ scripts/enums/poker/NumType.cs
@@ -0,0 +1,22 @@
+﻿//     定义卡牌数值类型的键值枚举
+namespace GFrameworkGodotTemplate.scripts.enums.poker;
+
+/// <summary>
+///     定义卡牌数值类型的键值枚举
+///     用于标识和管理不同的卡牌数值类型
+/// </summary>
+public enum NumType
+{
+    /// <summary>
+    ///     整数
+    /// </summary>
+    Integer,
+    /// <summary>
+    ///     分数
+    /// </summary>
+    Fraction,
+    /// <summary>
+    ///     小数
+    /// </summary>
+    Decimal
+}
