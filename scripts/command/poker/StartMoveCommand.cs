0a567b9cd2593481300428e0b3455bdb7c0235ed
Yumei1010 <3062181720@qq.com>
2026-02-15 12:10:22 +0800

    feat(command):添加扑克开始移动命令

---
+++ scripts/command/poker/StartMoveCommand.cs
@@ -0,0 +1,16 @@
+﻿using GFramework.Core.command;
+using GFramework.Core.extensions;
+using Godot;
+using Twentyfour.scripts.model;
+
+namespace Twentyfour.scripts.command.poker;
+
+public class StartMoveCommand : AbstractCommand
+{
+    protected override void OnExecute()
+    {
+        var model = this.GetModel<IPokerModel>()!;
+        model.IsMoving = true;
+        Input.SetMouseMode(Input.MouseModeEnum.ConfinedHidden);
+    }
+}
