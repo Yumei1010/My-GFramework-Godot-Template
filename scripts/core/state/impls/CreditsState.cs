fb61f0a2109e8d324edf01681973ed238f419ac1
Yumei1010 <3062181720@qq.com>
2026-03-23 17:27:36 +0800

    feat(state):添加制作菜单状态

---
+++ scripts/core/state/impls/CreditsState.cs
@@ -0,0 +1,39 @@
+﻿using GFramework.Core.Abstractions.state;
+using GFramework.Core.extensions;
+using GFramework.Core.state;
+using GFramework.Game.Abstractions.scene;
+using GFramework.Game.Abstractions.ui;
+using GFrameworkGodotTemplate.scripts.credits;
+
+namespace GFrameworkGodotTemplate.scripts.core.state.impls;
+
+/// <summary>
+///     计算菜单状态
+///     负责管理制作者界面的显示和隐藏逻辑
+/// </summary>
+public class CreditsState : ContextAwareStateBase
+{
+    /// <summary>
+    ///     状态进入时的处理方法
+    /// </summary>
+    /// <param name="from">从哪个状态切换过来，可能为空</param>
+    public override void OnEnter(IState? from)
+    {
+        // 回到计算菜单需要销毁其它所有Ui界面以及场景
+        var uiRouter = this.GetSystem<IUiRouter>()!;
+        uiRouter.Clear();
+        this.GetSystem<ISceneRouter>()!.Unload();
+        // 推送制作者UI到界面栈中，显示制作者界面
+        uiRouter.Push(Credits.UiKeyStr);
+    }
+
+    /// <summary>
+    ///     判断是否可以切换到下一个状态
+    /// </summary>
+    /// <param name="target">目标状态</param>
+    /// <returns>始终返回true，表示可以切换到任意状态</returns>
+    public override bool CanTransitionTo(IState target)
+    {
+        return true;
+    }
+}
